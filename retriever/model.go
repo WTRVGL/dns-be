09164d4f1aab5569d35c33b727d9110233145034
Wouter <wtrvngl@gmail.com>
2022-06-19 07:24:05 +0200

    Successful response struct

---
+++ retriever/model.go
@@ -0,0 +1,33 @@
+package retriever
+
+type DomainResponse struct {
+	DomainInfo struct {
+		Alabel             string `json:"alabel"`
+		Ulabel             string `json:"ulabel"`
+		Status             string `json:"status"`
+		Created            string `json:"created"`
+		Updated            string `json:"updated"`
+		DateAvailable      string `json:"dateAvailable"`
+		VerificationStatus string `json:"verificationStatus"`
+	}
+	NameserverInfo struct {
+		nameservers []struct {
+			Name      string `json:"name"`
+			NameASCII string `json:"nameASCII"`
+			Ip        string `json:"ip"`
+		}
+		Overridden          bool   `json:"Overridden"`
+		RegistryNsgroupName string `json:"registryNsgroupName"`
+	}
+	Registrant            interface{}   `json:"registrant"`
+	OnsiteCompanyContacts []interface{} `json:"onsiteCompanyContacts"`
+	TechContacts          []interface{} `json:"techContacts"`
+	DnsKeyInfo            struct {
+		DnsKeys interface{} `json:"dnsKeys"`
+	}
+	TransferProhibitedStatus struct {
+		TransferProhibited             bool `json:"transferProhibited"`
+		ServerTransferProhibited       bool `json:"ServerTransferProhibited"`
+		MaskedClientTransferProhibited bool `json:"MaskedClientTransferProhibited"`
+	}
+}
