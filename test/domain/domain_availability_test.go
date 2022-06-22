b949cd69cb333dce6b9be27c9489618ef2654c6f
Wouter <wtrvngl@gmail.com>
2022-06-22 02:38:30 +0200

    Test pointer removed

--- test/domain/domain_availability_test.go
+++ test/domain/domain_availability_test.go
@@ -9,7 +9,7 @@ import (
 func TestAvailableDomain(t *testing.T) {
 	domainName := "ifthisgetsregisteredthenripunittest.be"
 	domain, _ := checker.NewDomain(domainName)
-	*domain, _ = domain.CheckAvailability()
+	domain, _ = domain.CheckAvailability()
 	wantedAvailability := models.Availability{Status: "available", DateAvailable: ""}
 	if domain.Availability != wantedAvailability {
 		t.Errorf("CheckAvailability(\"ifthisgetsregisteredthenripunittest.be\"), got %s, want %s", domain.Availability, wantedAvailability)
@@ -19,7 +19,7 @@ func TestAvailableDomain(t *testing.T) {
 func TestUnavailableDomain(t *testing.T) {
 	domainName := "dnsbelgium.be"
 	domain, _ := checker.NewDomain(domainName)
-	*domain, _ = domain.CheckAvailability()
+	domain, _ = domain.CheckAvailability()
 	wantedAvailability := models.Availability{Status: "inuse", DateAvailable: ""}
 	if domain.Availability != wantedAvailability {
 		t.Errorf("CheckAvailability(\"dnsbelgium.be\"), got %s, want %s", domain, wantedAvailability)
